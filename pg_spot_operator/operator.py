249a7edc7ce99eb4b7492444abbe098f992cf40b
Kaarel Moppel <kaarel.moppel@gmail.com>
2024-10-06 11:53:26 +0300

    Fix --dry-run Ansible inventory compilation

--- pg_spot_operator/operator.py
+++ pg_spot_operator/operator.py
@@ -218,6 +218,8 @@ def generate_ansible_inventory_file_for_action(
     """Places an inventory file into temp_workdir"""
     if m.vm.host and m.vm.login_user:
         inventory = f"{m.vm.host} ansible_user={m.vm.login_user}"
+    elif dry_run:
+        inventory = "dummy"
     else:
         inventory = get_ansible_inventory_file_str_for_action(action, m.uuid, m.instance_name)  # type: ignore
     if not inventory:
