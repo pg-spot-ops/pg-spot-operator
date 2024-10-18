bd80b34e5fe7551b515388ddd88cfacb9169fa63
Kaarel Moppel <kaarel.moppel@gmail.com>
2024-10-18 11:03:31 +0300

    New manifest attribute / CLI flag --aws-vpc-id
    To be able to use non-default VPCs more conveniently. Previously once
    could already do it by specifying a Subnet ID - but this assumes also
    the AZ is set correctly by the user. Now we select the subnet according
    to the cheapest AZ found if just VPC ID specified

--- pg_spot_operator/cli.py
+++ pg_spot_operator/cli.py
@@ -118,6 +118,9 @@ class ArgumentParser(Tap):
     aws_security_group_ids: str = os.getenv(
         "PGSO_AWS_SECURITY_GROUP_IDS", ""
     )  # SG rules are "merged" if multiple provided
+    aws_vpc_id: str = os.getenv(
+        "PGSO_AWS_VPC_ID", ""
+    )  # If not set default VPC in region used
     aws_subnet_id: str = os.getenv("PGSO_AWS_SUBNET_ID", "")
     self_terminate_access_key_id: str = os.getenv(
         "PGSO_SELF_TERMINATE_ACCESS_KEY_ID", ""
@@ -212,6 +215,7 @@ def compile_manifest_from_cmdline_params(
         if args.aws_security_group_ids
         else []
     )
+    m.aws.vpc_id = args.aws_vpc_id
     m.aws.subnet_id = args.aws_subnet_id
     m.aws.access_key_id = args.aws_access_key_id
     m.aws.secret_access_key = args.aws_secret_access_key
@@ -394,6 +398,11 @@ def check_cli_args_valid(args: ArgumentParser):
         and not os.path.exists(os.path.expanduser(args.ansible_path))
     ):
         logger.error("--ansible-path %s not found", args.ansible_path)
+        exit(1)
+    if args.aws_vpc_id and not args.aws_vpc_id.startswith("vpc-"):
+        logger.error(
+            "Invalid --aws-vpc-id, expecting to start with 'vpc-'",
+        )
         exit(1)
 
 
