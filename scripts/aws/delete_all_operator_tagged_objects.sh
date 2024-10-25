b4b18c36a2b9a600c569c77266c8b884c84a8b45
Kaarel Moppel <kmoppel@users.noreply.github.com>
2024-10-25 06:36:13 +0300

    Refactor cleanup helper (#68)
    * Clean up resources region by region
    
    More logical so
    
    * Loop regions by sorted region name
    
    * Dont run the script without any parameters
    
    Show usage instead if no params given

--- scripts/aws/delete_all_operator_tagged_objects.sh
+++ scripts/aws/delete_all_operator_tagged_objects.sh
@@ -1,35 +1,51 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "Usage: delete_all_operator_tagged_objects.sh  yes/no (DRY RUN MODE)  ['eu-west-1 eu-north-1' OPTIONAL EXPLICIT REGION LIST]"
+  exit 0
+fi
+
 echo "Starting at `date`"
 set -e
 
 export PAGER=
 
-REGIONS=$(aws ec2 describe-regions --no-all-regions --query "Regions[*].[RegionName]" --output text)
-WET_RUN=
+WET_RUN=0
 
-if [ -n "$1" ]; then
-  WET_RUN=t
+if [ "${1:0:1}" == "y" ]; then
+  WET_RUN=1
 fi
 
-if [ -z "$WET_RUN" ]; then
-  echo "Running in DRY RUN mode ! Add any argument to actually delete"
+if [ -n "$2" ]; then
+  REGIONS="$2"
 else
+  REGIONS=$(aws ec2 describe-regions --no-all-regions --query "Regions[*].[RegionName]" --output text | sort)
+fi
+
+
+
+if [ "$WET_RUN" -gt 0 ]; then
   echo "WARNING - Deleting all 'pg-spot-operator-instance' tagged resources in regions $REGIONS"
   echo "sleep 5"
   sleep 5
+else
+  echo "Running in DRY RUN mode - just listing operator resources!"
 fi
 
 
 
-### VMs ###
-
-echo -e "\n*** Deleting AWS VMs ***"
-
-TERMINATED=0
+TOTAL_TERMINATED=0
 
 for reg in $REGIONS ; do
-echo -e "\nProcessing region: $reg\n"
+
+echo ""
+echo "*******************"
+echo "Processing region: $reg"
+echo "*******************"
+
+REGION_TERMINATED=0
+
+echo -e "\n*** Deleting AWS VMs ***"
 
 INSTANCES=$(aws ec2 describe-instances --region $reg --filters Name=tag-key,Values=pg-spot-operator-instance \
   Name=instance-state-name,Values=running,pending,stopped,stopping,shutting-down \
@@ -37,35 +53,31 @@ INSTANCES=$(aws ec2 describe-instances --region $reg --filters Name=tag-key,Valu
 
 for x in $INSTANCES ; do
   echo "Terminating VM $x ..."
-  if [ -n "$WET_RUN" ]; then
+  if [ "$WET_RUN" -gt 0 ]; then
     aws ec2 terminate-instances --region $reg --instance-ids $x >/dev/null
-    TERMINATED=$((TERMINATED+1))
+    REGION_TERMINATED=$((REGION_TERMINATED+1))
+    TOTAL_TERMINATED=$((TOTAL_TERMINATED+1))
   fi
 done
 
-done  # reg
-echo -e "\nDone. VMs terminated: ${TERMINATED}"
+echo -e "\nDone. VMs terminated in region: ${REGION_TERMINATED}"
 
-if [ "$TERMINATED" -gt 0 ]; then
+
+echo -e "\n*** Deleting AWS volumes ***"
+
+REGION_VOLUMES_TERMINATED=0
+
+VOLUMES=$(aws ec2 describe-volumes --region $reg --filters Name=tag-key,Values=pg-spot-operator-instance --query 'Volumes[*].VolumeId' --output text)
+
+if [ "$REGION_TERMINATED" -gt 0 ] && [ -n "$VOLUMES" ] ; then
   echo "Sleeping 5min before deleting volumes to give time to detach ..."
   sleep 300
 fi
 
 
-### VOLUMES ###
-
-echo -e "\n*** Deleting AWS volumes ***"
-
-TERMINATED=0
-
-for reg in $REGIONS ; do
-echo -e "\nProcessing region: $reg\n"
-
-VOLUMES=$(aws ec2 describe-volumes --region $reg --filters Name=tag-key,Values=pg-spot-operator-instance --query 'Volumes[*].VolumeId' --output text)
-
 for vol in $VOLUMES ; do
   echo "Terminating Volume $vol ..."
-  if [ -n "$WET_RUN" ]; then
+  if [ "$WET_RUN" -gt 0 ]; then
     vol_state=$(aws ec2 describe-volumes --region $reg --volume-id $vol --query "Volumes[*].State" --output text)
     echo "vol_state: $vol_state"
     if [ "$vol_state" != "available" ]; then
@@ -74,60 +86,62 @@ for vol in $VOLUMES ; do
     fi
     echo "aws ec2 delete-volume --region $reg --volume-id $vol >/dev/null"
     aws ec2 delete-volume --region $reg --volume-id $vol >/dev/null
-    TERMINATED=$((TERMINATED+1))
+    REGION_VOLUMES_TERMINATED=$((REGION_VOLUMES_TERMINATED+1))
   fi
 done
 
-done
-echo -e "\nDone. Volumes terminated: ${TERMINATED}"
+echo -e "\nDone. Volumes terminated: ${REGION_VOLUMES_TERMINATED}"
 
 
-### Elastic Public IPs
+
+
 
 echo -e "\n*** Deleting AWS Elastic IP Addresses ***"
 
-TERMINATED=0
-
-for reg in $REGIONS ; do
-echo -e "\nProcessing region: $reg\n"
+EIPS_TERMINATED=0
 
 ADDRESSES=$(aws ec2 describe-addresses --region $reg --filters Name=tag-key,Values=pg-spot-operator-instance --query 'Addresses[*].AllocationId' --output text)
 
 for aid in $ADDRESSES ; do
   echo "Terminating address $aid ..."
-  if [ -n "$WET_RUN" ]; then
+  if [ "$WET_RUN" -gt 0 ]; then
     aws ec2 release-address --region $reg --allocation-id $aid >/dev/null
-    TERMINATED=$((TERMINATED+1))
+    EIPS_TERMINATED=$((EIPS_TERMINATED+1))
   fi
 done
 
-done
-echo -e "\nDone. Addresses terminated: ${TERMINATED}"
+echo -e "\nDone. Addresses terminated: ${EIPS_TERMINATED}"
 
 
 
-### NICs
 
 echo -e "\n*** Deleting AWS network interfaces ***"
 
-TERMINATED=0
-
-for reg in $REGIONS ; do
-echo -e "\nProcessing region: $reg\n"
+NICS_TERMINATED=0
 
 NICS=$(aws ec2 describe-network-interfaces --region $reg --filters Name=tag-key,Values=pg-spot-operator-instance \
    Name=interface-type,Values=interface --query 'NetworkInterfaces[*].NetworkInterfaceId' --output text)
 
 for nic in $NICS ; do
   echo "Terminating NIC $nic ..."
-  if [ -n "$WET_RUN" ]; then
+  if [ "$WET_RUN" -gt 0 ]; then
     echo "aws ec2 delete-network-interface --region $reg --network-interface-id $nic >/dev/null"
     aws ec2 delete-network-interface --region $reg --network-interface-id $nic >/dev/null
-    TERMINATED=$((TERMINATED+1))
+    NICS_TERMINATED=$((NICS_TERMINATED+1))
   fi
 done
 
-done
-echo -e "Done. NICs terminated: ${TERMINATED}"
+echo -e "\nDone. NICs terminated: ${NICS_TERMINATED}"
 
-echo "Finished at `date`"
+
+
+
+
+echo -e "\nDone with region ${reg}\n"
+
+done  # reg
+
+echo -e "All regions processed"
+echo "Total VMs deleted: $TOTAL_TERMINATED"
+
+echo -e "\nFinished at `date`"
