ef3352362a9b2b289ff6a1ea921363a811777a07
Kaarel Moppel <kmoppel@users.noreply.github.com>
2024-11-11 15:32:47 +0200

    Example Terraform to create a sandbox VPC + IAM user + creds (#80)
    IAM policies are region limited for additional play safety

---
+++ scripts/terraform/create-iam-user-and-credentials/create_region_limited_user.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+locals {
+  user = "pg-spot-operator"
+  region = "eu-north-1"
+}
+
+resource "aws_iam_user" "user" {
+  name          = local.user
+}
+
+resource "aws_iam_user_policy" "user_policy" {
+  name = "pg-spot-operator-region-limited"
+  user = aws_iam_user.user.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+          "pricing:*",
+          "cloudwatch:ListMetrics",
+          "cloudwatch:GetMetricStatistics",
+          "cloudwatch:Describe*",
+          "ec2:RunInstances",
+          "ec2:TerminateInstances",
+          "ec2:StartInstances",
+          "ec2:StopInstances",
+          "ec2:CreateTags",
+          "ec2:CreateNetworkInterface",
+          "ec2:DeleteNetworkInterface",
+          "ec2:AllocateAddress",
+          "ec2:DisassociateAddress",
+          "ec2:*Volume*",
+          "s3:ListBucket*",
+          "s3:ListMultipart*",
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:DeleteObject",
+          "s3:DeleteObjectVersion"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+         Condition = {
+            "StringEquals" = {
+              "aws:RequestedRegion": local.region
+            }
+          }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_access_key" "user_access_key" {
+  user       = local.user
+  depends_on = [aws_iam_user.user]
+}
+
+output "credentials" {
+  value = {
+    "key"      = aws_iam_access_key.user_access_key.id
+    "secret"   = aws_iam_access_key.user_access_key.secret
+  }
+  sensitive = true
+}
+
+# terraform output -json
---
+++ scripts/terraform/create-vpc/create_vpc.tf
@@ -0,0 +1,56 @@
+# https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest#usage
+# REPLACE eu-north-1 with your region of choice
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "pgspotops"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = false
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+resource "aws_default_security_group" "default_sg" {
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+# Feed to pg_spot_operator via: --aws-vpc-id=xxx
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
